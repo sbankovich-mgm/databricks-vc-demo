1a57bbbcd7131de690908ab2d32e9befa522f7b9
sbankovich@mgmresorts.com <sbankovich@mgmresorts.com>
2023-07-25 21:43:08 +0000

    adding dependent notebook & get param file

---
+++ dependent-task-notebook.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %run "./utils"
+
+# COMMAND ----------
+
+test_param = get_param('test_param', "default value")
+
+# COMMAND ----------
+
+print(test_param)
--- utils.py
+++ utils.py
@@ -10,3 +10,13 @@ print('this is the utils notebook!')
 
 def get_random_number():
     return random.random()
+
+# COMMAND ----------
+
+def get_param(param_name, default):
+    try:
+        p = dbutils.widgets.get(param_name)
+    except:
+        p = default
+    print(f"param {param_name} run with value {p}")
+    return p
